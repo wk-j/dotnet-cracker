d9895c00f069f10fed6f810fc580484759e3d81c
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-12-13 20:35:26 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Try Code Cracker ###
+
+- https://github.com/code-cracker/code-cracker
+
+```bash
+dotnet add src/Hello/Hello.csproj package CodeCracker.CSharp
+dotnet build src/Hello/Hello.csproj
+```
---
+++ src/Hello/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Hello {
+    class Person {
+        public int Age { set; get; }
+        public String Name { set; get; }
+    }
+
+    class Program {
+        string k_k;
+        static string uuuu_____;
+        static void Main(string[] args) {
+            var array = new[] { 1, 2, 3 };
+            for (int i = 0; i < array.Length; i++) {
+                var someNumber = array[i];
+                //some code
+            }
+
+            /// bbb
+            string s = "1000";
+            Console.WriteLine(s);
+
+            /// aaa
+            Console.WriteLine(uuuu_____);
+            Console.WriteLine("Hello World!");
+
+            Person p;
+            p = new Person();
+            p.Name = "Mahmoud";
+            p.Age = 25;
+        }
+    }
+}
